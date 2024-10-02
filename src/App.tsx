c2fa04a789de3efced426df8a074fe6f2f087287
Nick Coughlin <coughlin.nicholas@gmail.com>
2024-10-01 21:24:39 -0700

    updated api endpoint to new format with correct stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // URL of the API endpoint
+  const url =
+    "https://emzdc3lzy9.execute-api.us-east-1.amazonaws.com/dev/sample-data";
+
+  // Function to get data from the API using fetch
+  const getData = async () => {
+    try {
+      const response = await fetch(url);
+
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Data: ", data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  // Call the function to fetch data
+  getData();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Gummicube + Ncoughlin</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
