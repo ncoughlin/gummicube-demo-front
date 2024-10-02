de7b30379e66c415e25914a3bd950a0c580146f8
Nick Coughlin <coughlin.nicholas@gmail.com>
2024-10-02 11:03:26 -0700

    intro text scaffold

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -31,19 +31,19 @@ function App() {
       <div className="min-h-screen  flex flex-col items-center">
         <div className="w-full max-w-6xl flex flex-col items-center">
           <Spacer vertical={10} verticalMobile={2} />
-          <img src={gummicubexncoughlin} className="max-w-xl"/>
-          <Spacer vertical={10} verticalMobile={2} />
-          <h1 className="text-3xl font-bold mb-4 text-center">
-            Welcome to My Website
-          </h1>
-          <p className="text-gray-700 text-center mb-4">
-            This is a simple single-page website built using Vite and
-            TailwindCSS. The content is centered horizontally and arranged in a
-            column.
+          <img src={gummicubexncoughlin} className="max-w-xl" />
+          <Spacer vertical={6} verticalMobile={2} />
+          <p className="max-w-2xl">
+            Dave,
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700 mx-auto">
-            Click Me
-          </button>
         </div>
       </div>
     </>
