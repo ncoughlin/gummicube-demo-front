d48f5c2fe0a9b128d14b0560769088d42b2c3b59
Nick Coughlin <coughlin.nicholas@gmail.com>
2024-10-02 15:07:29 -0700

    basic page is done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,19 +9,21 @@ function App() {
     <div className="min-h-screen  flex flex-col items-center">
       <div className="w-full max-w-6xl flex flex-col items-center">
         <Spacer vertical={10} verticalMobile={2} />
-        <img src={gummicubexncoughlin} className="max-w-md" />
+        <img src={gummicubexncoughlin} className="max-w-sm" />
         <Spacer vertical={6} verticalMobile={2} />
-        <p className="max-w-2xl">
-          Dave,
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="max-w-4xl">
+          <p className=" text-xl">
+            This page is intended to demonstrate a basic proficiency with the
+            <strong> Pulumi </strong>infrastructure as code framework. The data
+            that populates the chart below is being served from a back-end
+            created entirely with Pulumi. It includes an API Gateway, Lambda and
+            RDS PostgreSQL database.
+          </p>
+          <p className=" text-xl">
+            The source code for the project can be found in the Code Sandbox
+            below.
+          </p>
+        </div>
         <Spacer vertical={6} verticalMobile={2} />
         <SampleDataDisplay />
         <Spacer vertical={6} verticalMobile={2} />
